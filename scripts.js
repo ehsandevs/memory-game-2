2927a22294d1c95916a37934a396ac8cd6f27680
Amir.E Haji Hussaini <niamileo@gmail.com>
2019-12-02 08:28:10 +0330

    initilize matching

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let FirstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        FirstCard = this;
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
